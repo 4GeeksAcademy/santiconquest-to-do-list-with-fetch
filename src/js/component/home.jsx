242579055a0b1d0f57252fd234c95efc5bc4c75d
Santiconquest <161180110+Santiconquest@users.noreply.github.com>
2024-09-21 14:53:48 +0000

    Se agregan botones para probar API

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,18 +2,65 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+function getTask() {
+	console.log("Presionaaste el boton Get Task")
+	fetch("https://playground.4geeks.com/todo/users/Lebron")
+	.then((response)=>response.json())
+	.then((data)=>console.log(data.todos))
+}
+function addTask() {
+	console.log("Presionaaste el boton Add Task")
+	const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(
+			{
+				"label": "Ir a la cancha",
+				"is_done": false,
+			})
+    };
+    fetch('https://playground.4geeks.com/todo/todos/Lebron', requestOptions)
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
+function deleteTask() {
+	console.log("Presionaaste el boton Delete Task")
+	const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(
+			{
+				"label": "Ir a la cancha",
+				"is_done": false,
+			})
+    };
+	fetch("https://playground.4geeks.com/todo/todos/21",
+		requestOptions
+	)
+	.then((response) => response.text())
+	.then((result) => console.log(result))
+}
+//create your first component
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
+			<div>
+				<h1>Lista de tareas</h1>
+				<ul>
+					<li>
+						<input 
+							type="text" 
+							placeholder="Ingrese tarea a realizar"
+						/>
+					</li>
+				</ul>
+			</div>
+
+			<button onClick={getTask}>Get Task</button>
+			<button onClick={addTask}>Add Task</button>
+			<button onClick={deleteTask}>Delete Task</button>
+
+
 			<p>
 				Made by{" "}
 				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
